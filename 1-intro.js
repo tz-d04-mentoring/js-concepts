cdd10ee7f0a3f0df11832a5609ee2b175926af43
losrobbos <50549469+losrobbos@users.noreply.github.com>
2025-02-28 15:27:06 +0100

    Update 1-intro.js

--- 1-intro.js
+++ 1-intro.js
@@ -12,9 +12,9 @@ console.log(pw)
 // prüfe contents => VALIDATION
 // FUNKTION von Javascript nehmen zum überprüfen von Content
 const checkEmail = email.includes("@")
-console.log(checkEmail)
+console.log(checkEmail) // gibt "true" aus, also "wahr" => das heißt, @ ist in der Variable "email" enthalten. Wenn es nicht enthalten ist, ergibt der Check "false"
 
-const checkPw = pw.length > 8 // berechne etwas und speicher ERGEBNIS es in der Variablen checkPw
+const checkPw = pw.length > 8 // berechne etwas und speicher ERGEBNIS in der Variablen checkPw
 console.log(checkPw)
 
 
